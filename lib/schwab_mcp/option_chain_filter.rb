f9686afd2056c5792e3defc2ab39b8f34f3048cd
Joseph Platta <jwplatta@gmail.com>
2025-07-13 16:27:07 -0500

    add an option chain filter utility

---
+++ lib/schwab_mcp/option_chain_filter.rb
@@ -0,0 +1,176 @@
+# frozen_string_literal: true
+require_relative "loggable"
+
+module SchwabMCP
+  class OptionChainFilter
+    include Loggable
+
+    attr_reader :expiration_date, :underlying_price, :expiration_type,
+                :settlement_type, :option_root, :max_delta, :max_spread,
+                :min_credit, :min_open_interest, :dist_from_strike, :quantity
+
+    def initialize(
+      expiration_date:,
+      underlying_price:,
+      expiration_type: nil,
+      settlement_type: nil,
+      option_root: nil,
+      max_delta: 0.15,
+      max_spread: 20.0,
+      min_credit: 0.0,
+      min_open_interest: 0,
+      dist_from_strike: 0.0,
+      quantity: 1
+    )
+      @expiration_date = expiration_date
+      @underlying_price = underlying_price
+      @expiration_type = expiration_type
+      @settlement_type = settlement_type
+      @option_root = option_root
+      @max_delta = max_delta
+      @max_spread = max_spread
+      @min_credit = min_credit
+      @min_open_interest = min_open_interest
+      @dist_from_strike = dist_from_strike
+      @quantity = quantity
+    end
+
+    def find_spread_candidates(options_map, option_type)
+      spreads = []
+      exp_date_str = expiration_date.strftime('%Y-%m-%d')
+
+      short_cnt = 0
+
+      options_map.each do |date_key, strikes|
+        next unless date_matches?(date_key, exp_date_str)
+        log_debug("Processing options for date: #{date_key}. Searching #{strikes.size} strikes...")
+
+        strikes.each do |strike_key, option_array|
+          option_array.each do |short_option|
+            next unless passes_short_option_filters?(short_option)
+
+            log_debug("Found short option: #{short_option[:symbol]} at strike #{short_option[:strikePrice]}")
+
+            short_cnt += 1
+
+            short_strike = short_option[:strikePrice]
+            short_mark = short_option[:mark]
+
+            long_options = find_long_option_candidates(strikes, short_option, option_type)
+
+            long_options.each do |long_option|
+              spread = build_spread(short_option, long_option)
+              spreads << spread if spread
+            end
+          end
+        end
+      end
+
+      log_debug("Found #{spreads.size} #{option_type} spreads for #{short_cnt} short options")
+
+      spreads
+    end
+
+    def passes_short_option_filters?(option)
+      return false unless passes_delta_filter?(option)
+      return false unless passes_open_interest_filter?(option)
+      return false unless passes_distance_filter?(option)
+      return false unless passes_optional_filters?(option)
+
+      true
+    end
+
+    private
+
+    def date_matches?(date_key, exp_date_str)
+      date_key.to_s.include?(exp_date_str)
+    end
+
+    def passes_delta_filter?(option)
+      delta = option[:delta]&.abs || 0
+      delta <= max_delta && delta >= 0.01
+    end
+
+    def passes_open_interest_filter?(option)
+      open_interest = option[:openInterest] || 0
+      open_interest >= min_open_interest
+    end
+
+    def passes_distance_filter?(option)
+      strike = option[:strikePrice]
+      return false unless strike
+
+      distance = ((underlying_price - strike) / underlying_price).abs
+      distance >= dist_from_strike
+    end
+
+    def passes_optional_filters?(option)
+      return false if expiration_type && option[:expirationType] != expiration_type
+      return false if settlement_type && option[:settlementType] != settlement_type
+      return false if option_root && option[:optionRoot] != option_root
+
+      true
+    end
+
+    def find_long_option_candidates(strikes, short_option, option_type)
+      short_strike = short_option[:strikePrice]
+      candidates = []
+
+      strikes.each do |long_strike_key, long_option_array|
+        long_option_array.each do |long_option|
+          long_strike = long_option[:strikePrice]
+          long_mark = long_option[:mark]
+
+          next unless long_mark > 0
+          next unless valid_spread_structure?(short_strike, long_strike, option_type)
+          next unless passes_min_credit?(short_option, long_option)
+          next unless passes_optional_filters?(long_option)
+          next unless passes_open_interest_filter?(long_option)
+
+          candidates << long_option
+        end
+      end
+
+      candidates
+    end
+
+    def valid_spread_structure?(short_strike, long_strike, option_type)
+      case option_type
+      when 'call'
+        long_strike > short_strike && (long_strike - short_strike) <= max_spread
+
+      when 'put'
+        long_strike < short_strike && (short_strike - long_strike) <= max_spread
+      else
+        false
+      end
+    end
+
+    def passes_min_credit?(short_option, long_option)
+      return true if min_credit <= 0
+
+      short_mark = short_option[:mark]
+      long_mark = long_option[:mark]
+      credit = short_mark - long_mark
+
+      credit * 100 >= min_credit
+    end
+
+    def build_spread(short_option, long_option)
+      short_mark = short_option[:mark]
+      long_mark = long_option[:mark]
+      credit = short_mark - long_mark
+      short_strike = short_option[:strikePrice]
+      long_strike = long_option[:strikePrice]
+
+      {
+        short_option: short_option,
+        long_option: long_option,
+        credit: credit,
+        delta: short_option[:delta] || 0,
+        spread_width: (short_strike - long_strike).abs,
+        quantity: quantity
+      }
+    end
+  end
+end
---
+++ spec/option_chain_filter_spec.rb
@@ -0,0 +1,352 @@
+# frozen_string_literal: true
+
+require 'date'
+require 'spec_helper'
+require_relative '../lib/schwab_mcp/option_chain_filter'
+
+RSpec.describe SchwabMCP::OptionChainFilter do
+  let(:expiration_date) { Date.new(2025, 1, 17) }
+  let(:underlying_price) { 5800.0 }
+
+  let(:filter) do
+    described_class.new(
+      expiration_date: expiration_date,
+      underlying_price: underlying_price,
+      max_delta: 0.15,
+      max_spread: 20.0,
+      min_credit: 100.0,
+      min_open_interest: 10,
+      dist_from_strike: 0.05
+    )
+  end
+
+  describe '#initialize' do
+    it 'creates a filter with the given parameters' do
+      expect(filter.expiration_date).to eq(expiration_date)
+      expect(filter.underlying_price).to eq(underlying_price)
+      expect(filter.max_delta).to eq(0.15)
+      expect(filter.max_spread).to eq(20.0)
+      expect(filter.min_credit).to eq(100.0)
+      expect(filter.min_open_interest).to eq(10)
+      expect(filter.dist_from_strike).to eq(0.05)
+    end
+  end
+
+  describe '#passes_short_option_filters?' do
+    let(:base_option) do
+      {
+        delta: -0.10,
+        openInterest: 50,
+        strikePrice: 5500.0,
+        mark: 5.50
+      }
+    end
+
+    context 'with valid option parameters' do
+      it 'returns true for an option that meets all criteria' do
+        expect(filter.passes_short_option_filters?(base_option)).to be true
+      end
+    end
+
+    context 'with invalid delta' do
+      it 'returns false when delta is too high (absolute value)' do
+        high_delta_option = base_option.merge(delta: -0.25)
+        expect(filter.passes_short_option_filters?(high_delta_option)).to be false
+      end
+
+      it 'returns true for positive delta (delta filter only checks absolute value)' do
+        positive_delta_option = base_option.merge(delta: 0.10)
+        expect(filter.passes_short_option_filters?(positive_delta_option)).to be true
+      end
+    end
+
+    context 'with insufficient open interest' do
+      it 'returns false when open interest is below minimum' do
+        low_oi_option = base_option.merge(openInterest: 5)
+        expect(filter.passes_short_option_filters?(low_oi_option)).to be false
+      end
+    end
+
+    context 'with strike price too close to underlying' do
+      it 'returns false when strike is within distance threshold' do
+        close_strike_option = base_option.merge(strikePrice: 5790.0)
+        expect(filter.passes_short_option_filters?(close_strike_option)).to be false
+      end
+
+      it 'returns true when strike is far enough from underlying' do
+        far_strike_option = base_option.merge(strikePrice: 5500.0)
+        expect(filter.passes_short_option_filters?(far_strike_option)).to be true
+      end
+    end
+  end
+
+  describe '#find_spread_candidates' do
+    let(:sample_options_map) do
+      {
+        "2025-01-17:2" => {
+          "5500.0" => [{
+            delta: -0.10,
+            openInterest: 50,
+            strikePrice: 5500.0,
+            mark: 5.50
+          }],
+          "5490.0" => [{
+            delta: -0.08,
+            openInterest: 30,
+            strikePrice: 5490.0,
+            mark: 4.25
+          }],
+          "5480.0" => [{
+            delta: -0.06,
+            openInterest: 25,
+            strikePrice: 5480.0,
+            mark: 3.75
+          }],
+          "5470.0" => [{
+            delta: -0.04,
+            openInterest: 15,
+            strikePrice: 5470.0,
+            mark: 2.50
+          }]
+        }
+      }
+    end
+
+    context 'with put spreads' do
+      it 'finds valid put spread combinations' do
+        put_filter = described_class.new(
+          expiration_date: expiration_date,
+          underlying_price: underlying_price,
+          max_delta: 0.15,
+          max_spread: 20.0,
+          min_credit: 1.0, # Lower minimum for testing
+          min_open_interest: 10,
+          dist_from_strike: 0.05
+        )
+
+        spreads = put_filter.find_spread_candidates(sample_options_map, 'put')
+
+        expect(spreads).not_to be_empty
+        expect(spreads.length).to be >= 1
+
+        # Check first spread structure
+        first_spread = spreads.first
+        expect(first_spread).to have_key(:short_option)
+        expect(first_spread).to have_key(:long_option)
+        expect(first_spread).to have_key(:credit)
+        expect(first_spread).to have_key(:spread_width)
+
+        # Verify it's a proper put spread (short strike > long strike)
+        expect(first_spread[:short_option][:strikePrice]).to be > first_spread[:long_option][:strikePrice]
+
+        # Verify credit is positive
+        expect(first_spread[:credit]).to be > 0
+      end
+
+      it 'calculates correct credit for spreads' do
+        # Create filter with lower min_credit for testing
+        put_filter = described_class.new(
+          expiration_date: expiration_date,
+          underlying_price: underlying_price,
+          max_delta: 0.15,
+          max_spread: 20.0,
+          min_credit: 1.0, # Lower minimum for testing
+          min_open_interest: 10,
+          dist_from_strike: 0.05
+        )
+
+        spreads = put_filter.find_spread_candidates(sample_options_map, 'put')
+
+        # Find the 5500/5490 spread
+        spread_5500_5490 = spreads.find do |spread|
+          spread[:short_option][:strikePrice] == 5500.0 &&
+          spread[:long_option][:strikePrice] == 5490.0
+        end
+
+        expect(spread_5500_5490).not_to be_nil
+        expect(spread_5500_5490[:credit]).to eq(1.25) # 5.50 - 4.25
+        expect(spread_5500_5490[:spread_width]).to eq(10.0) # 5500 - 5490
+      end
+    end
+
+    context 'with call spreads' do
+      let(:call_options_map) do
+        {
+          "2025-01-17:1" => {
+            "6100.0" => [{
+              delta: 0.10,
+              openInterest: 50,
+              strikePrice: 6100.0,
+              mark: 5.50
+            }],
+            "6110.0" => [{
+              delta: 0.08,
+              openInterest: 30,
+              strikePrice: 6110.0,
+              mark: 4.25
+            }],
+            "6120.0" => [{
+              delta: 0.06,
+              openInterest: 25,
+              strikePrice: 6120.0,
+              mark: 3.75
+            }]
+          }
+        }
+      end
+
+      it 'finds valid call spread combinations' do
+        # Create filter with lower min_credit and dist_from_strike for call spreads
+        call_filter = described_class.new(
+          expiration_date: expiration_date,
+          underlying_price: underlying_price,
+          max_delta: 0.15,
+          max_spread: 20.0,
+          min_credit: 1.0, # Lower minimum for call spread test
+          min_open_interest: 10,
+          dist_from_strike: 0.04 # Lower distance requirement
+        )
+
+        spreads = call_filter.find_spread_candidates(call_options_map, 'call')
+
+        expect(spreads).not_to be_empty
+
+        # Verify it's a proper call spread (short strike < long strike)
+        first_spread = spreads.first
+        expect(first_spread[:short_option][:strikePrice]).to be < first_spread[:long_option][:strikePrice]
+      end
+    end
+
+    context 'with no valid options' do
+      let(:invalid_options_map) do
+        {
+          "2025-01-17:2" => {
+            "5790.0" => [{
+              delta: -0.10,
+              openInterest: 5, # Too low open interest
+              strikePrice: 5790.0, # Too close to underlying
+              mark: 5.50
+            }]
+          }
+        }
+      end
+
+      it 'returns empty array when no options pass filters' do
+        spreads = filter.find_spread_candidates(invalid_options_map, 'put')
+        expect(spreads).to be_empty
+      end
+    end
+  end
+
+  describe 'filtering behavior' do
+    context 'delta filtering' do
+      it 'accepts options with delta within threshold' do
+        valid_option = { delta: 0.10, openInterest: 50, strikePrice: 5500.0, mark: 5.50 }
+        expect(filter.passes_short_option_filters?(valid_option)).to be true
+      end
+
+      it 'rejects options with delta exceeding threshold' do
+        high_delta_option = { delta: 0.20, openInterest: 50, strikePrice: 5500.0, mark: 5.50 }
+        expect(filter.passes_short_option_filters?(high_delta_option)).to be false
+      end
+
+      it 'handles negative delta correctly' do
+        negative_delta_option = { delta: -0.10, openInterest: 50, strikePrice: 5500.0, mark: 5.50 }
+        expect(filter.passes_short_option_filters?(negative_delta_option)).to be true
+      end
+    end
+
+    context 'minimum credit filtering' do
+      let(:high_credit_filter) do
+        described_class.new(
+          expiration_date: expiration_date,
+          underlying_price: underlying_price,
+          max_delta: 0.15,
+          max_spread: 20.0,
+          min_credit: 200.0, # High minimum credit
+          min_open_interest: 10,
+          dist_from_strike: 0.05
+        )
+      end
+
+      it 'accepts spreads that meet minimum credit requirement' do
+        short_option = { mark: 6.00, strikePrice: 5500.0, delta: -0.10 }
+        long_option = { mark: 4.00, strikePrice: 5490.0, delta: -0.08 }
+
+        expect(high_credit_filter.send(:passes_min_credit?, short_option, long_option)).to be true
+      end
+
+      it 'rejects spreads below minimum credit requirement' do
+        short_option = { mark: 5.50, strikePrice: 5500.0, delta: -0.10 }
+        long_option = { mark: 4.25, strikePrice: 5490.0, delta: -0.08 }
+
+        expect(high_credit_filter.send(:passes_min_credit?, short_option, long_option)).to be false
+      end
+
+      it 'allows all spreads when min_credit is 0' do
+        zero_credit_filter = described_class.new(
+          expiration_date: expiration_date,
+          underlying_price: underlying_price,
+          max_delta: 0.15,
+          max_spread: 20.0,
+          min_credit: 0.0,
+          min_open_interest: 10,
+          dist_from_strike: 0.05
+        )
+
+        short_option = { mark: 1.00, strikePrice: 5500.0, delta: -0.10 }
+        long_option = { mark: 0.90, strikePrice: 5490.0, delta: -0.08 }
+
+        expect(zero_credit_filter.send(:passes_min_credit?, short_option, long_option)).to be true
+      end
+    end
+
+    context 'date matching' do
+      it 'finds spreads for matching expiration dates' do
+        matching_options = {
+          "2025-01-17:2" => {
+            "5500.0" => [{ delta: -0.10, openInterest: 50, strikePrice: 5500.0, mark: 5.50 }],
+            "5490.0" => [{ delta: -0.08, openInterest: 30, strikePrice: 5490.0, mark: 4.25 }]
+          }
+        }
+
+        spreads = filter.find_spread_candidates(matching_options, 'put')
+        expect(spreads).not_to be_empty
+      end
+
+      it 'ignores options with non-matching expiration dates' do
+        non_matching_options = {
+          "2025-01-24:2" => {
+            "5500.0" => [{ delta: -0.10, openInterest: 50, strikePrice: 5500.0, mark: 5.50 }],
+            "5490.0" => [{ delta: -0.08, openInterest: 30, strikePrice: 5490.0, mark: 4.25 }]
+          }
+        }
+
+        spreads = filter.find_spread_candidates(non_matching_options, 'put')
+        expect(spreads).to be_empty
+      end
+
+      it 'works with symbol keys (as returned by JSON.parse with symbolize_names)' do
+        symbol_key_options = {
+          :"2025-01-17:2" => {
+            "5500.0" => [{ delta: -0.10, openInterest: 50, strikePrice: 5500.0, mark: 5.50 }],
+            "5490.0" => [{ delta: -0.08, openInterest: 30, strikePrice: 5490.0, mark: 4.25 }]
+          }
+        }
+
+        test_filter = described_class.new(
+          expiration_date: expiration_date,
+          underlying_price: underlying_price,
+          max_delta: 0.15,
+          max_spread: 20.0,
+          min_credit: 1.0,
+          min_open_interest: 10,
+          dist_from_strike: 0.05
+        )
+
+        spreads = test_filter.find_spread_candidates(symbol_key_options, 'put')
+        expect(spreads).not_to be_empty
+      end
+    end
+  end
+end
