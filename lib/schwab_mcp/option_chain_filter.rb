513d0b2027efb6021051ba3857f4914f2ba59eb0
Joseph Platta <jwplatta@gmail.com>
2025-07-15 15:21:54 -0500

    add a select method to the option chain filter to select options for a delta and strike range

--- lib/schwab_mcp/option_chain_filter.rb
+++ lib/schwab_mcp/option_chain_filter.rb
@@ -7,35 +7,60 @@ module SchwabMCP
 
     attr_reader :expiration_date, :underlying_price, :expiration_type,
                 :settlement_type, :option_root, :max_delta, :max_spread,
-                :min_credit, :min_open_interest, :dist_from_strike, :quantity
+                :min_credit, :min_open_interest, :dist_from_strike, :quantity,
+                :min_delta, :min_strike, :max_strike
 
     def initialize(
       expiration_date:,
-      underlying_price:,
+      underlying_price: nil,
       expiration_type: nil,
       settlement_type: nil,
       option_root: nil,
+      min_delta: 0.0,
       max_delta: 0.15,
       max_spread: 20.0,
       min_credit: 0.0,
       min_open_interest: 0,
       dist_from_strike: 0.0,
-      quantity: 1
+      quantity: 1,
+      max_strike: nil,
+      min_strike: nil
     )
       @expiration_date = expiration_date
       @underlying_price = underlying_price
       @expiration_type = expiration_type
       @settlement_type = settlement_type
       @option_root = option_root
-      @max_delta = max_delta
       @max_spread = max_spread
       @min_credit = min_credit
       @min_open_interest = min_open_interest
       @dist_from_strike = dist_from_strike
       @quantity = quantity
+      @max_delta = max_delta
+      @min_delta = min_delta
+      @max_strike = max_strike
+      @min_strike = min_strike
     end
 
-    def find_spread_candidates(options_map, option_type)
+    def select(options_map)
+      filtered_options = []
+      exp_date_str = expiration_date.strftime('%Y-%m-%d')
+
+      options_map.each do |date_key, strikes|
+        strikes.each do |strike_key, option_array|
+          option_array.each do |option|
+            next unless passes_delta_filter?(option)
+            next unless passes_strike_range_filter?(option)
+            filtered_options << option
+          end
+        end
+      end
+
+      log_debug("Found #{filtered_options.size} filtered options")
+      filtered_options
+    end
+
+    def find_spreads(options_map, option_type)
       spreads = []
       exp_date_str = expiration_date.strftime('%Y-%m-%d')
 
@@ -87,8 +112,8 @@ module SchwabMCP
     end
 
     def passes_delta_filter?(option)
-      delta = option[:delta]&.abs || 0
-      delta <= max_delta && delta >= 0.01
+      delta = option[:delta]&.abs || 0.0
+      delta <= max_delta && delta >= min_delta
     end
 
     def passes_open_interest_filter?(option)
@@ -97,6 +122,8 @@ module SchwabMCP
     end
 
     def passes_distance_filter?(option)
+      raise "Underlying price must be set for distance filter" unless underlying_price
+
       strike = option[:strikePrice]
       return false unless strike
 
@@ -108,6 +135,16 @@ module SchwabMCP
       return false if expiration_type && option[:expirationType] != expiration_type
       return false if settlement_type && option[:settlementType] != settlement_type
       return false if option_root && option[:optionRoot] != option_root
+
+      true
+    end
+
+    def passes_strike_range_filter?(option)
+      strike = option[:strikePrice]
+      return false unless strike
+
+      return false if @min_strike && strike < @min_strike
+      return false if @max_strike && strike > @max_strike
 
       true
     end
